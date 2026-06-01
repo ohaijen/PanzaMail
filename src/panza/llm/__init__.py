3c2c462ace18a6c0fbc7f038f8f24bd727c53dc9
Jen Iofinova <eugenia.iofinova@ist.ac.at>
2026-06-01 14:55:14 +0200

    add MLX llm to __init__

--- src/panza/llm/__init__.py
+++ src/panza/llm/__init__.py
@@ -1,5 +1,14 @@
 from .base import LLM, ChatHistoryType, MessageType
 from .local import PeftLLM, TransformersLLM
+from .mlx import MLXLLM
 from .ollama import OllamaLLM
 
-__all__ = ["LLM", "ChatHistoryType", "MessageType", "OllamaLLM", "TransformersLLM", "PeftLLM"]
+__all__ = [
+    "LLM",
+    "ChatHistoryType",
+    "MessageType",
+    "OllamaLLM",
+    "TransformersLLM",
+    "PeftLLM",
+    "MLXLLM",
+]
